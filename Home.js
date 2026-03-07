32a6cd1ac3d4067229d9f469b38041a06a0e6511
duartexz-dev <duarteturzim11@gmail.com>
2026-03-07 11:08:05 -0300

    Sistema de login / sistema de perfil criados.

---
+++ Home.js
@@ -0,0 +1,58 @@
+let users = [
+    {
+        nome: "Sou o Primeiro User", idade: 123, id: 1
+    }
+]
+
+function confirmar() {
+
+
+
+    let nome = document.getElementById("nome").value
+    let idade = Number(document.getElementById("idade").value)
+    let res = document.getElementById("result")
+    let res2 = document.getElementById("res2")
+    let res3 = document.getElementById("res3")
+    let res4 = document.getElementById("res4")
+
+
+    if (nome && idade >= 18 && idade <= 60) {
+
+        res.innerHTML = `${nome} seu dados foram confirmados , já pode continuar !`
+
+    } else if (!nome || !idade) {
+
+        alert("Coloque seu dados COMPLETOS por favor!")
+
+    } else if (idade < 18) {
+
+
+        res.innerHTML = `Aqui não vende doce criança!`
+
+
+    }
+
+
+    users.push({
+
+        nome: nome,
+        idade: idade,
+        id: users.length + 1
+
+    },
+    )
+    res2.innerHTML = `
+  Nome: ${nome}
+  `
+
+    res3.innerHTML = `
+  Idade: ${idade}
+  `
+    res4.innerHTML = `
+  Id: ${users.length}
+  `
+    console.log(users)
+
+
+
+}
